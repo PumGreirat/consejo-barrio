a09143f24cfb8dff33d99556b7d194a2ad80d9bf
PumGreirat <eldmx.98@gmail.com>
2026-04-08 23:29:38 -0600

    2

--- components/CouncilView.tsx
+++ components/CouncilView.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React from 'react'
 import { useState } from 'react'
 import { Report, SECTIONS, ORGS, getMuType, isBishopric, Profile } from '@/types'
 import { createClient } from '@/lib/supabase'
